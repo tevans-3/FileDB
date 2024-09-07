9690e5a6325a8d6c80883ba475a98315f7032c98
tevans-3 <156032013+tevans-3@users.noreply.github.com>
2024-09-07 11:53:55 -0600

    Update in_progress.py

--- in_progress.py
+++ in_progress.py
@@ -17,7 +17,7 @@ class in_progress(employee):
             shutil.copy(file_to_upload, self.orig_address+new_fnm)
             shutil.move(self.orig_address, dir)
             
-            result = f'Successfully uploaded {fnm} to {self.ID} - {self.firstname} {self.lastname} HR records.\nSuccessfully removed {self.ID}""s record from "In Progress" and uploaded to Active directory."
+            result = f'Successfully uploaded {fnm} to {self.ID} - {self.firstname} {self.lastname} records.\nSuccessfully removed {self.ID}""s record from "In Progress" and uploaded to Active directory."
             with open(success_log, 'a') as f:
                 f.write(result+'\n')    
             return result
